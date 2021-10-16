2ed80c77881e57d8ee52590fd8e231aa26217cbf
Julius Altmüller <julius.altmueller@gamil.com>
2021-10-16 17:53:24 +0200

    neue funktionen definiert

--- T-Wert.py
+++ T-Wert.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import math
+import scipy.integrate
+import scipy.stats
+import numpy as np
 
 Data = pd.read_table(r'Schublehre vs Schublehre.txt')
 
@@ -29,17 +32,18 @@ def T_Wert(values1, values2): #Funktion zur Berechnung des T-Werts
 
     return math.sqrt(n/((stanDev(values1))**2+(stanDev(values2))**2))*(mean(values1)-mean(values2))
 
+def tdist(f, t):
+
+     return math.gamma((f+1)/2)/(math.sqrt(f*math.pi)*math.gamma(f/2))*(1+t**2/f)**(-(f+1)/2)
+
+def pvalue(f):
+
+    return scipy.integrate.quad(tdist, T_Wert(RoterSteinAnalog, RoterSteinDigital), 10, args=(f))
+
 #zwei Seitiger T-Test n+m-2 Freiheitsgrade
 
 #Signifikantsniveau = 0.05
 
-print(T_Wert(RoterSteinAnalog, RoterSteinDigital))
+print(T_Wert(RoterSteinDigital, RoterSteinAnalog))
 
-print(mean(RoterSteinAnalog))
-print(stanDev(RoterSteinAnalog))
-
-print(mean(RoterSteinDigital))
-print(stanDev(RoterSteinDigital))
-
-#Hello World
-
+print(scipy.stats.ttest_rel(RoterSteinDigital, RoterSteinAnalog))
